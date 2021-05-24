21b5953cae9a0db89d61d7a916ebd50b343b7e03
sergey <petrelevich@yandex.ru>
2021-05-24 20:18:42 +0300

    L15-structuralPatterns

--- L15-structuralPatterns/homework/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ L15-structuralPatterns/homework/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -2,24 +2,18 @@ package ru.otus.listener.homework;
 
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 public class HistoryListener implements Listener, HistoryReader {
 
-    private final Map<Long, Message> messages = new HashMap<>();
-
     @Override
     public void onUpdated(Message msg) {
-        messages.put(msg.getId(), msg.toBuilder().build());
         //throw new UnsupportedOperationException();
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        return Optional.of(messages.get(id));
         //throw new UnsupportedOperationException();
+        return null;
     }
 }
